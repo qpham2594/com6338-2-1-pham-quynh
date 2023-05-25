7e046aa1b3c4cab05104648d73d4c36479d8cb63
Quynh <qpham2594@gmail.com>
2023-05-25 11:28:46 -0400

    greetings tests

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+function greet () {
+    let sign = prompt ("What is your name?")
+    console.log (sign)
+    alert ("Hello " + sign)
+
+    let age = prompt ("How old are you?")
+    console.log (parseInt(age)) //Only if the number is starting the answer, will give NaN value is the words are in front of the value; Only looks at the first value starting the string"
+    
+   // confirm ("Have your birthday already passed this year?")
+   // alert ("If you already had a birthday, then you were born in " + ((new Date().getFullYear()) - age))
+   // alert ("If you have NOT had a birthday yet, then you were born in " + ((new Date().getFullYear()) - age + 1))
+
+
+    if (confirm) {
+        var retVal =  confirm ("Have your birthday already passed this year?");
+        if (retVal === true)
+        {alert ("If you already had a birthday, then you were born in " + ((new Date().getFullYear()) - age))}
+
+    else{ 
+        (retVal === false)
+        alert ("If you have NOT had a birthday yet, then you were born in " + ((new Date().getFullYear()) - age + 1))}
+    }
+    
+}
